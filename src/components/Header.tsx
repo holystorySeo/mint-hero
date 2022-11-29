8aeb6d79eaaf48282ecbc21a857d390ff33dec32
holystorySeo <holystory.seo@gmail.com>
2022-11-29 23:35:34 +0900

    style: Add z-index

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,9 +7,9 @@ export default function Header() {
 const HeaderWeb = styled.div`
   width: 100%;
   height: 80px;
-  /* position: fixed;
+  position: fixed;
   top: 0;
   left: 0;
-  z-index: 99; */
+  z-index: 99;
   border: 1px solid black;
 `;
