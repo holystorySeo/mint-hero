2e94f84cb70b61849cfa47ec4bc07530784542a2
holystorySeo <holystoryseo@gmail.com>
2022-11-29 15:12:11 +0900

    feat: Add minting page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Page from "./page/Page";
+import { InjectedConnector } from "@web3-react/injected-connector";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Page />
     </div>
   );
 }
 
 export default App;
+
+export const Injected = new InjectedConnector({});
